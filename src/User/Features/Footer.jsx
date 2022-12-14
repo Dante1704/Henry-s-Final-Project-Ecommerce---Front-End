97029c7c73fe0d096e3d3ee57ae3a2c3e44258c3
Andreh-Z <witecan931@gmail.com>
2022-12-14 15:40:49 -0300

    footer_Fixed

--- src/User/Features/Footer.jsx
+++ src/User/Features/Footer.jsx
@@ -15,10 +15,12 @@ export default function Footer() {
     <footer className=" inset-x-0 bottom-0 p-5 bg-gray-900 sm:p-6 dark:bg-gray-900">
       <div className="md:flex md:justify-between">
         <div className="mb-6 md:mb-0">
-          <a href="asdas" className="flex items-center">
+          <a
+            href="https://comforting-chebakia-ea0479.netlify.app/"
+            className="flex items-center"
+          >
             {/* <img src="asdasd" className="mr-3 h-8" alt="Icono nuestro" /> */}
             <span className="self-center text-2xl font-cursive-titles font-semibold whitespace-nowrap text-white">
-              Tienda Nuestra
               <img src={logo} />
             </span>
           </a>
