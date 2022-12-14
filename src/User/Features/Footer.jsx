6370614a2c23a959c08e9a0337e4b222e84b9256
Dante1704 <dante.nkaddarian@gmail.com>
2022-12-14 17:25:03 -0300

    footer modificado

--- src/User/Features/Footer.jsx
+++ src/User/Features/Footer.jsx
@@ -12,13 +12,12 @@ import logo from "../../Images/logo.png";
 
 export default function Footer() {
   return (
-    <footer className=" inset-x-0 bottom-0 p-5 bg-gray-900 sm:p-6 dark:bg-gray-900">
+    <footer className="inset-x-0 bottom-0 p-5 bg-gray-900 sm:p-6 dark:bg-gray-900">
       <div className="md:flex md:justify-between">
         <div className="mb-6 md:mb-0">
           <a href="asdas" className="flex items-center">
             {/* <img src="asdasd" className="mr-3 h-8" alt="Icono nuestro" /> */}
             <span className="self-center text-2xl font-cursive-titles font-semibold whitespace-nowrap text-white">
-              Tienda Nuestra
               <img src={logo} />
             </span>
           </a>
